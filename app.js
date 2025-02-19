7ca0dd237712e5bb4e82d85a1d49b44b06a77568
Orifha <orifhatshikotshi1@gmail.com>
2025-02-19 22:25:46 +0200

    test transaction endpoint

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello there", app: "owes" });
+app.use(express.json());
+
+const transactions = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data.json`)
+);
+
+app.get("/api/v1/transactions", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: transactions.length,
+    data: {
+      transactions,
+    },
+  });
+});
+
+app.post("/api/v1/transactions", (req, res) => {
+  // console.log(req.body);
+  const newId = transactions[transactions.length - 1].id + 1;
+
+  const newTransaction = Object.assign({ id: newId }, req.body);
+
+  transactions.push(newTransaction);
+  fs.writeFile(
+    `${__dirname}/dev-data/data.json`,
+    JSON.stringify(transactions),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          transaction: newTransaction,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
