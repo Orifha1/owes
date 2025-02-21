3f191fd0206cab6726aa11a1aa6ab70511f5e33e
Orifha <orifhatshikotshi1@gmail.com>
2025-02-21 19:52:58 +0200

    add routes

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
+const morgan = require("morgan");
 const express = require("express");
 
 const app = express();
 
+// MIDDLEWARES
+app.use(morgan("dev"));
 app.use(express.json());
 
 const transactions = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data.json`)
 );
 
-app.get("/api/v1/transactions", (req, res) => {
+// ROUTE HANDLERS
+const getAllTransactions = (req, res) => {
   res.status(200).json({
     status: "success",
     results: transactions.length,
@@ -17,9 +21,9 @@ app.get("/api/v1/transactions", (req, res) => {
       transactions,
     },
   });
-});
+};
 
-app.get("/api/v1/transactions/:id", (req, res) => {
+const getTransaction = (req, res) => {
   const id = req.params.id * 1;
   const transaction = transactions.find((el) => el.id === id);
   if (!transaction) {
@@ -35,10 +39,9 @@ app.get("/api/v1/transactions/:id", (req, res) => {
       transaction,
     },
   });
-});
+};
 
-app.post("/api/v1/transactions", (req, res) => {
-  // console.log(req.body);
+const createTransaction = (req, res) => {
   const newId = transactions[transactions.length - 1].id + 1;
 
   const newTransaction = Object.assign({ id: newId }, req.body);
@@ -56,9 +59,9 @@ app.post("/api/v1/transactions", (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch("/api/v1/transactions/:id", (req, res) => {
+const updateTransaction = (req, res) => {
   if (req.params.id * 1 > transactions.length) {
     return res.status(404).json({
       status: "fail",
@@ -71,9 +74,9 @@ app.patch("/api/v1/transactions/:id", (req, res) => {
       transaction: "<updated transaction here>",
     },
   });
-});
+};
 
-app.delete("/api/v1/transactions/:id", (req, res) => {
+const deleteTransaction = (req, res) => {
   if (req.params.id * 1 > transactions.length) {
     return res.status(404).json({
       status: "fail",
@@ -84,8 +87,58 @@ app.delete("/api/v1/transactions/:id", (req, res) => {
     status: "success",
     data: null,
   });
-});
+};
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+const createUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not yet defined",
+  });
+};
+
+// ROUTES
+const transactionRouter = express.Router();
+const userRouter = express.Router();
+
+transactionRouter.route("/").get(getAllTransactions).post(createTransaction);
+
+transactionRouter
+  .route("/:id")
+  .get(getTransaction)
+  .patch(updateTransaction)
+  .delete(deleteTransaction);
+
+userRouter.route("/").get(getAllUsers).post(createUsers);
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/transactions", transactionRouter);
+app.use("/api/v1/users", userRouter);
+
+// START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
