573a2c03a0729a419d1751bfc31658c2ed2b4839
Orifha <orifhatshikotshi1@gmail.com>
2025-02-22 16:37:52 +0200

    set up mongo

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const morgan = require("morgan");
-const express = require("express");
-const transactionRouter = require("./routes/transactionsRoutes");
-const userRouter = require("./routes/userRoutes");
+const morgan = require('morgan');
+const express = require('express');
+const transactionRouter = require('./routes/transactionsRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // MIDDLEWARES
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
 // ROUTES
-app.use("/api/v1/transactions", transactionRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/transactions', transactionRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
