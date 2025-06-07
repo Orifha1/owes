691961839f79a8bd5e11f57f47cd34efdfca57fd
Orifha <orifhatshikotshi1@gmail.com>
2025-06-07 18:33:01 +0200

    add apifeatures class

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,52 +1,19 @@
 const Transaction = require('./../models/transactionModel');
+const APIFeatues = require('./../utils/apiFeatures');
 
 /**
  * Gets all the transactions.
  */
 exports.getAllTransactions = async (req, res) => {
   try {
-    // BUILD THE QUERY
-    // Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Transaction.find(JSON.parse(queryStr));
-
-    // Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numTransactions = await Transaction.countDocuments();
-      if (skip >= numTransactions) {
-        throw new Error('This page does not exist');
-      }
-    }
+    const features = new APIFeatues(Transaction.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
     // EXECUTE THE QUERY
-    const transactions = await query;
+    const transactions = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
