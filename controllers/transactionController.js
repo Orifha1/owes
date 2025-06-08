53b6486fd0c10efa32feff40728d45b06a363b23
Orifha <orifhatshikotshi1@gmail.com>
2025-06-08 16:30:53 +0200

    add catching errors in async functions

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,169 +1,124 @@
 const Transaction = require('./../models/transactionModel');
 const APIFeatues = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
 
 /**
  * Gets all the transactions.
  */
-exports.getAllTransactions = async (req, res) => {
-  try {
-    const features = new APIFeatues(Transaction.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+exports.getAllTransactions = catchAsync(async (req, res, next) => {
+  const features = new APIFeatues(Transaction.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    // EXECUTE THE QUERY
-    const transactions = await features.query;
+  // EXECUTE THE QUERY
+  const transactions = await features.query;
 
-    // SEND RESPONSE
-    res.status(200).json({
-      status: 'success',
-      results: transactions.length,
-      data: {
-        transactions,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: transactions.length,
+    data: {
+      transactions,
+    },
+  });
+});
 
 /**
  * Gets a specific transaction
  */
-exports.getTransaction = async (req, res) => {
+exports.getTransaction = catchAsync(async (req, res, next) => {
   const id = req.params.id;
 
-  try {
-    const transaction = await Transaction.findById(id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        transaction,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  const transaction = await Transaction.findById(id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      transaction,
+    },
+  });
+});
 
 /**
  * Creates the user transaction that will be shared by ythe users.
  */
-exports.createTransaction = async (req, res) => {
-  try {
-    const newTransaction = await Transaction.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        transaction: newTransaction,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.createTransaction = catchAsync(async (req, res, next) => {
+  const newTransaction = await Transaction.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      transaction: newTransaction,
+    },
+  });
+});
 
 /**
  * Updates the trancaction.
  */
-exports.updateTransaction = async (req, res) => {
+exports.updateTransaction = catchAsync(async (req, res, next) => {
   const id = req.params.id;
-  try {
-    const updatedTransaction = await Transaction.findByIdAndUpdate(
-      id,
-      req.body,
-      {
-        new: true, // return newly updated transaction.
-        runValidators: true,
-      },
-    );
-    res.status(200).json({
-      status: 'success',
-      data: {
-        transaction: updatedTransaction,
-      },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  const updatedTransaction = await Transaction.findByIdAndUpdate(id, req.body, {
+    new: true, // return newly updated transaction.
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      transaction: updatedTransaction,
+    },
+  });
+});
 
 /**
  * Deletes a specific transaction.
  */
-exports.deleteTransaction = async (req, res) => {
-  try {
-    const id = req.params.id;
-    await Transaction.findByIdAndDelete(id);
+exports.deleteTransaction = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+  await Transaction.findByIdAndDelete(id);
 
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
-exports.getTransactionsStats = async (req, res) => {
-  try {
-    const stats = await Transaction.aggregate([
-      {
-        $match: {
-          amount: { $gte: 1 },
+exports.getTransactionsStats = catchAsync(async (req, res, next) => {
+  const stats = await Transaction.aggregate([
+    {
+      $match: {
+        amount: { $gte: 1 },
+      },
+    },
+    {
+      $group: {
+        _id: '$createdAt',
+        numTransactions: {
+          $sum: 1,
+        },
+        numAmount: {
+          $sum: '$amount',
+        },
+        avgAmount: {
+          $avg: '$amount',
+        },
+        minAmount: {
+          $min: '$amount',
+        },
+        maxAmount: {
+          $max: '$amount',
         },
       },
-      {
-        $group: {
-          _id: '$createdAt',
-          numTransactions: {
-            $sum: 1,
-          },
-          numAmount: {
-            $sum: '$amount',
-          },
-          avgAmount: {
-            $avg: '$amount',
-          },
-          minAmount: {
-            $min: '$amount',
-          },
-          maxAmount: {
-            $max: '$amount',
-          },
-        },
+    },
+    {
+      $sort: {
+        avgAmount: 1,
       },
-      {
-        $sort: {
-          avgAmount: 1,
-        },
-      },
-    ]);
+    },
+  ]);
 
-    res.status(200).json({
-      status: 'success',
-      stats,
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    stats,
+  });
+});
